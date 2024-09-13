ad7822e8c37bc902b55c05e97913b31df4c9866f
Asami Doi <doiasami1219@gmail.com>
2024-09-13 21:04:45 +0200

    update run_on_wasabi.sh

--- ch6/saba/run_on_wasabi.sh
+++ ch6/saba/run_on_wasabi.sh
@@ -3,10 +3,11 @@
 HOME_PATH=$PWD
 TARGET_PATH=$PWD"/build"
 OS_PATH=$TARGET_PATH"/wasabi"
+# アプリケーションの名前がsabaとは異なるとき、次の行を変更する
 APP_NAME="saba"
-APP_PATH=$OS_PATH"/app/"$APP_NAME
+MAKEFILE_PATH=$HOME_PATH"/Makefile"
 
-# build ディレクトリを作成する
+# buildディレクトリを作成する
 if [ -d $TARGET_PATH ]
 then
   echo $TARGET_PATH" exists"
@@ -15,17 +16,15 @@ else
   mkdir $TARGET_PATH
 fi
 
-# WasabiOSをダウンロードする (https://github.com/hikalium/wasabi)
-# もしスクリプトが失敗する場合は、`rm -rf build/wasabi`などでダウンロードしたOSを削除する必要がある
+# WasabiOSをダウンロードする（https://github.com/hikalium/wasabi）
+# もしスクリプトが失敗する場合は、`rm -rf build/wasabi`などで
+# ダウンロードしたOSを削除する必要がある
 if [ -d $OS_PATH ]
 then
   echo $OS_PATH" exists"
   echo "pulling new changes..."
   cd $OS_PATH
-  git fetch --all
-  git branch backup
-  git reset --hard origin/main
-  git branch -D backup
+  git pull
 else
   echo $OS_PATH" doesn't exist"
   echo "cloning wasabi project..."
@@ -33,34 +32,14 @@ else
   git clone git@github.com:hikalium/wasabi.git
 fi
 
+# アプリケーションのトップディレクトリに移動する
 cd $HOME_PATH
 
-# build/wasabi/app/saba/ディレクトリを作成する
-if [ -d $APP_PATH ]
-then
-  echo $APP_PATH" exists"
-else
-  echo $APP_PATH" doesn't exist"
-  mkdir $APP_PATH
+# Makefileをダウンロードする
+if [ ! -f $MAKEFILE_PATH ]; then
+  echo "downloading Makefile..."
+  wget https://raw.githubusercontent.com/hikalium/wasabi/main/external_app_template/Makefile
 fi
 
-# アプリケーションのコードをbuild/wasabi/app/saba/ディレクトリ以下にコピーする
-echo "copying the project to wasabi OS..."
-cp -R `ls -A ./ | grep -v "target" | grep -v ".git" | grep -v "build"` $APP_PATH
-
-cd $OS_PATH
-
-# Cargo.tomlの[workspace]にメンバーを追加する
-mv Cargo.toml Cargo.toml.original
-if [ $(grep -c "app/$APP_NAME" Cargo.toml.original) -eq 1 ]
-then
-  echo "$APP_PATH already exists in Cargo.toml"
-  mv Cargo.toml.original Cargo.toml
-else
-  sed "s/^members = \[/members = \[\n    \"app\/$APP_NAME\",/" Cargo.toml.original >| Cargo.toml
-fi
-rm Cargo.toml.original
-
-make run
-
-cd $HOME_PATH
+make build
+$OS_PATH/scripts/run_with_app.sh ./target/x86_64-unknown-none/release/$APP_NAME
