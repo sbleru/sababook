9ba5d370e9b5288b94e8b2e927c210d4f6ae6cad
Asami Doi <doiasami1219@gmail.com>
2024-09-15 19:26:34 +0200

    ch7

---
+++ ch7/saba/saba_core/src/renderer/dom/api.rs
@@ -0,0 +1,87 @@
+use crate::renderer::dom::node::Element;
+use crate::renderer::dom::node::ElementKind;
+use crate::renderer::dom::node::Node;
+use crate::renderer::dom::node::NodeKind;
+use alloc::rc::Rc;
+use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+
+pub fn get_element_by_id(
+    node: Option<Rc<RefCell<Node>>>,
+    id_name: &String,
+) -> Option<Rc<RefCell<Node>>> {
+    match node {
+        Some(n) => {
+            if let NodeKind::Element(e) = n.borrow().kind() {
+                for attr in &e.attributes() {
+                    if attr.name() == "id" && attr.value() == *id_name {
+                        return Some(n.clone());
+                    }
+                }
+            }
+            let result1 = get_element_by_id(n.borrow().first_child(), id_name);
+            let result2 = get_element_by_id(n.borrow().next_sibling(), id_name);
+            if result1.is_none() {
+                return result2;
+            }
+            result1
+        }
+        None => None,
+    }
+}
+
+pub fn get_target_element_node(
+    node: Option<Rc<RefCell<Node>>>,
+    element_kind: ElementKind,
+) -> Option<Rc<RefCell<Node>>> {
+    match node {
+        Some(n) => {
+            if n.borrow().kind()
+                == NodeKind::Element(Element::new(&element_kind.to_string(), Vec::new()))
+            {
+                return Some(n.clone());
+            }
+            let result1 = get_target_element_node(n.borrow().first_child(), element_kind);
+            let result2 = get_target_element_node(n.borrow().next_sibling(), element_kind);
+            if result1.is_none() {
+                return result2;
+            }
+            result1
+        }
+        None => None,
+    }
+}
+
+pub fn get_style_content(root: Rc<RefCell<Node>>) -> String {
+    let style_node = match get_target_element_node(Some(root), ElementKind::Style) {
+        Some(node) => node,
+        None => return "".to_string(),
+    };
+    let text_node = match style_node.borrow().first_child() {
+        Some(node) => node,
+        None => return "".to_string(),
+    };
+    let content = match &text_node.borrow().kind() {
+        NodeKind::Text(ref s) => s.clone(),
+        _ => "".to_string(),
+    };
+    content
+}
+
+pub fn get_js_content(root: Rc<RefCell<Node>>) -> String {
+    let js_node = match get_target_element_node(Some(root), ElementKind::Script) {
+        Some(node) => node,
+        None => return "".to_string(),
+    };
+    let text_node = match js_node.borrow().first_child() {
+        Some(node) => node,
+        None => return "".to_string(),
+    };
+    let content = match &text_node.borrow().kind() {
+        NodeKind::Text(ref s) => s.clone(),
+        _ => "".to_string(),
+    };
+    content
+}
---
+++ ch7/saba/saba_core/src/renderer/page.rs
@@ -0,0 +1,135 @@
+use crate::browser::Browser;
+use crate::display_item::DisplayItem;
+use crate::http::HttpResponse;
+use crate::renderer::css::cssom::CssParser;
+use crate::renderer::css::cssom::StyleSheet;
+use crate::renderer::css::token::CssTokenizer;
+use crate::renderer::dom::api::get_js_content;
+use crate::renderer::dom::api::get_style_content;
+use crate::renderer::dom::node::ElementKind;
+use crate::renderer::dom::node::NodeKind;
+use crate::renderer::dom::node::Window;
+use crate::renderer::html::parser::HtmlParser;
+use crate::renderer::html::token::HtmlTokenizer;
+use crate::renderer::js::ast::JsParser;
+use crate::renderer::js::runtime::JsRuntime;
+use crate::renderer::js::token::JsLexer;
+use crate::renderer::layout::layout_view::LayoutView;
+use alloc::rc::Rc;
+use alloc::rc::Weak;
+use alloc::string::String;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+
+#[derive(Debug, Clone)]
+pub struct Page {
+    browser: Weak<RefCell<Browser>>,
+    frame: Option<Rc<RefCell<Window>>>,
+    style: Option<StyleSheet>,
+    layout_view: Option<LayoutView>,
+    display_items: Vec<DisplayItem>,
+}
+
+impl Page {
+    pub fn new() -> Self {
+        Self {
+            browser: Weak::new(),
+            frame: None,
+            style: None,
+            layout_view: None,
+            display_items: Vec::new(),
+        }
+    }
+
+    pub fn clicked(&self, position: (i64, i64)) -> Option<String> {
+        let view = match &self.layout_view {
+            Some(v) => v,
+            None => return None,
+        };
+
+        if let Some(n) = view.find_node_by_position(position) {
+            if let Some(parent) = n.borrow().parent().upgrade() {
+                if let NodeKind::Element(e) = parent.borrow().node_kind() {
+                    if e.kind() == ElementKind::A {
+                        return e.get_attribute("href");
+                    }
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn set_browser(&mut self, browser: Weak<RefCell<Browser>>) {
+        self.browser = browser;
+    }
+
+    pub fn receive_response(&mut self, response: HttpResponse) {
+        self.create_frame(response.body());
+
+        self.execute_js();
+
+        self.set_layout_view();
+
+        self.paint_tree();
+    }
+
+    fn execute_js(&mut self) {
+        let dom = match &self.frame {
+            Some(frame) => frame.borrow().document(),
+            None => return,
+        };
+
+        let js = get_js_content(dom.clone());
+        let lexer = JsLexer::new(js);
+
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+
+        let mut runtime = JsRuntime::new(dom);
+        runtime.execute(&ast);
+    }
+
+    fn create_frame(&mut self, html: String) {
+        let html_tokenizer = HtmlTokenizer::new(html);
+        let frame = HtmlParser::new(html_tokenizer).construct_tree();
+        let dom = frame.borrow().document();
+
+        let style = get_style_content(dom);
+        let css_tokenizer = CssTokenizer::new(style);
+        let cssom = CssParser::new(css_tokenizer).parse_stylesheet();
+
+        self.frame = Some(frame);
+        self.style = Some(cssom);
+    }
+
+    fn set_layout_view(&mut self) {
+        let dom = match &self.frame {
+            Some(frame) => frame.borrow().document(),
+            None => return,
+        };
+
+        let style = match self.style.clone() {
+            Some(style) => style,
+            None => return,
+        };
+
+        let layout_view = LayoutView::new(dom, &style);
+
+        self.layout_view = Some(layout_view);
+    }
+
+    fn paint_tree(&mut self) {
+        if let Some(layout_view) = &self.layout_view {
+            self.display_items = layout_view.paint();
+        }
+    }
+
+    pub fn display_items(&self) -> Vec<DisplayItem> {
+        self.display_items.clone()
+    }
+
+    pub fn clear_display_items(&mut self) {
+        self.display_items = Vec::new();
+    }
+}
